4e8bc2c3c7f588e9fbf36d63000ebac870cb3aba
RullyBagjaaa <reallybagjaa@gmail.com>
2024-04-01 11:05:03 +0700

    terakhiran lanjut habis tidur ngoook

--- shelter.js
+++ shelter.js
@@ -20,3 +20,12 @@ previewBoxes.forEach(preview => {
         previewContainer.style.display = 'none';
     };
 }); 
+
+window.addEventListener('scroll', function() {
+    const navbar = document.querySelector('.navbar');
+    if (window.scrollY > 0) {
+      navbar.classList.add('scrolled');
+    } else {
+      navbar.classList.remove('scrolled');
+    }
+  });
